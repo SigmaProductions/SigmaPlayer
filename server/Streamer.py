b0d61a091b5d19267b8f2bf60a913c98721748dd
nynon <nynonmaslo@gmail.com>
2020-06-25 23:06:50 +0200

    first commit

--- server/Streamer.py
+++ server/Streamer.py
@@ -3,7 +3,8 @@ import time
 
 vlc = vlc.Instance()
 player = vlc.media_player_new()
-media = vlc.media_new('./server/test.mp4')
+media = vlc.media_new('./test.mp4')
 player.set_media(media)
 player.play()
 time.sleep(25)
+print("dobre tam")
